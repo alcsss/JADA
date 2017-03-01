73137974b6553ff73c21eb6671735a32397c69ee
robles1993 <robles1993r@gmail.com>
2017-03-02 00:25:31 +0100

    Javascript en html

---
+++ web/js/codigo.js
@@ -0,0 +1,10 @@
+var i=0;
+function funcionboton(){
+	i=i+1;
+	var boton=document.getElementById("ebotona");
+	ebotona.value="presione aqui("+i+")";
+	alert(i);
+	var condicion=i;
+	document.getElementById("bo").innerHTML="El botón si funciona";
+	
+}
