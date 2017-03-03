e02c899734108fdcad855233a13957e6b84a4ce2
MrYou <morala92@hotmail.com>
2017-03-03 14:34:45 +0100

    Mant.sh y Pasos_previos.sh

--- bash/mant.sh
+++ bash/mant.sh
@@ -1,15 +1,21 @@
-# Actualizar repositorio
+# Actualizar repositorio // añadir opcion "hard reset" para forzar la actualizacion
 cd /home/pi/nerv
 git pull
 cd
 
-# Copiar web en directorio web
+# Copiar web en directorio web // Revisar permisos
 sudo rm -r /var/www/html/*
 sudo cp -r /home/pi/nerv/web/* /var/www/html/
-sudo chown -R www-data:www-data /var/www/html/*
+sudo chown -R pi:www-data /var/www/html/*
+sudo find /var/www/html/ -type f -exec chmod 644 {} \; && sudo find /var/www/html/ -type d -exec chmod 755 {} \;
 
 # Reiniciar servicios
 sudo service lighttpd restart
 
-# Cargar el sketch en el arduino
-sudo platformio run -t upload -d ./nerv/arduino/
+# Cargar el sketch en el arduino // Usar variable $1 para cargarlo
+if [ $1 = "a" ];
+	then
+		sudo platformio run -t upload -d ./nerv/arduino/
+fi
+
+echo " -- Mantenimiento finalizado -- "
--- bash/pasos_previos.sh
+++ bash/pasos_previos.sh
@@ -1,12 +1,13 @@
-# Nos situamos en la carpeta del usuario pi
+# Nos situamos en la carpeta home, seguramente /home/pi
 cd
 
-# Crear archivo de configuracion con variables (nombre, tipo de arduino) para que las usen otros scripts
-# touch config.cfg  
+# Crear archivo de configuracion con variables (nombre, tipo de arduino) 
+# para que las usen otros scripts
+# touch config.cfg
 
 # Actualizar la raspberry e instalar los programas necesarios
 sudo apt-get update -y
-sudo apt-get install -y python-serial samba lighttpd git python3 screen
+sudo apt-get install -y python-serial samba lighttpd git python3 screen php5-cgi
 
 #Instalar platformIO
 python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
@@ -14,12 +15,29 @@ python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/
 # Otorgar permisos necesarios para cargar sketch
 sudo usermod -a -G dialout pi
 
-# copiar los archivos de configuracion por defecto si fuese necesario // cp .
-# lighttpd
-
-
 # Descargar el repositorio desde GitHub
 git clone https://github.com/alcsss/nerv
+
+# Copiar los archivos de configuracion por defecto si fuese necesario
+# -- Lighttpd --
+# sudo mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.old
+# cp ./nerv/bash/configs/lighttpd.conf /etc/lighttpd/lighttpd.conf
+# -- Samba --
+# sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.old
+# cp ./nerv/bash/configs/smb.conf /etc/samba/smb.conf
+#
+
+# Cambiar configuraciones
+# -- Nombre de red -- // Poner nombre segun variable
+sudo echo "raspi" > /etc/hostname 
+
+# -- Carpeta compartida -- // Compartir en red local la carpeta home
+# guest account = pi
+# 
+
+# Reiniciar servicios
+sudo service lighttpd restart
+sudo service samba restart
 
 # Crear los enlaces a los archivos 
 ln -s ./nerv/bash/mant.sh mant.sh
@@ -27,8 +45,8 @@ ln -s ./nerv/python/prueba.py prueba.py
 ls -l 
 
 # Ejecutar script de mantenimiento
-sudo bash ./mant.sh
+bash ./mant.sh
 
-echo "---------------- Instalacion realizada ----------------"
+echo " ---------------- Instalacion realizada ---------------- "
 echo "En la primera carga de sketch se instalaran archivos necesarios"
 echo "Reinicie para asegurarse de que las configuraciones se cargan correctamente"
